09cfed594e210e6f8faf202c11e8f9eea8143992
paulina szylin <paulina.szylin.programista@gmail.com>
2022-03-30 01:25:19 +0200

    Created new login page

---
+++ public/js/panel.js
@@ -0,0 +1,51 @@
+// MOTIVATIONAL STUFF PANEL
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const motivation_panel_container = document.querySelector('.motivation-panel-container');
+
+floating_btn.addEventListener('click', () => {
+    motivation_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+    motivation_panel_container.classList.remove('visible')
+});
+
+//not logged users - stack of cards
+
+$(document).ready(function () {
+
+    var content = $('.content');
+    var currentItem = content.filter('.active');
+    var steps = $('.card').filter('.steps');
+    var inactive1 = $('.inactive-1');
+    var inactive2 = $('.inactive-2');
+
+    $('.button').click(function () {
+        var nextItem = currentItem.next();
+        var lastItem = content.last();
+        var contentFirst = content.first();
+
+        currentItem.removeClass('active');
+
+        if (currentItem.is(lastItem)) {
+            currentItem = contentFirst.addClass('active');
+            currentItem.css({ 'right': '10%', 'opacity': '1' });
+            $('.step').animate({ width: '33%' });
+            inactive1.animate({ height: '8px', marginLeft: '20px', marginRight: '20px' }, 100);
+            inactive2.animate({ height: '8px', marginLeft: '10px', marginRight: '10px' }, 100);
+
+        } else if (currentItem.is(contentFirst)) {
+            currentItem.animate({ opacity: 0 }, 1000);
+            currentItem = nextItem.addClass('active');
+            $('.step').animate({ width: '66%' });
+            inactive2.animate({ height: '0', marginLeft: '0px', marginRight: '0px' }, 100);
+
+        } else {
+            currentItem = nextItem.addClass('active');
+            $('.step').animate({ width: '100%' });
+            inactive1.animate({ height: '0', marginLeft: '0px', marginRight: '0px' }, 100);
+        }
+    });
+
+});
