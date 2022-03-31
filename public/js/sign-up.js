1892cdd0126a568250f2df98a6306b494d489be1
paulina szylin <paulina.szylin.programista@gmail.com>
2022-03-31 13:40:23 +0200

    Adjusted jquery validation for multiple steps signup form

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -1,26 +1,24 @@
-$.validator.addMethod('validPassword',
-    function (value, element, param) {
-        if (value != '') {
-            if (value.match(/.*[a-z]+.*/i) == null) {
-                return false;
-            }
-            if (value.match(/.*\d+.*/) == null) {
-                return false;
-            }
-        }
-
-        return true;
-    },
-    'Hasło musi zawierać przynajmniej 1 literę i 1 cyfrę.'
-);
-
 
 $(document).ready(function () {
 
-    /**
-     * Validate the form
-     */
-    $('.formSignup').validate({
+    $.validator.addMethod('validPassword',
+        function (value, element, param) {
+            if (value != '') {
+                if (value.match(/.*[a-z]+.*/i) == null) {
+                    return false;
+                }
+                if (value.match(/.*\d+.*/) == null) {
+                    return false;
+                }
+            }
+
+            return true;
+        },
+        'Hasło musi zawierać przynajmniej 1 literę i 1 cyfrę.'
+    );
+
+
+    var v = $("#msform").validate({
         rules: {
             name: 'required',
             email: {
@@ -39,8 +37,39 @@ $(document).ready(function () {
                 email: 'Podaj poprawny adres e-mail',
                 remote: 'Podany adres e-mail istnieje już w bazie danych.'
             }
+        },
+        errorElement: "span",
+        errorClass: "help-inline",
+    });
+
+
+
+    $(".open1").click(function () {
+        if (v.form()) {
+            $("#sf2").show("slow");
         }
     });
+
+    // Binding next button on second step
+    $(".open2").click(function () {
+        if (v.form()) {
+            $("#sf3").show("slow");
+        }
+    });
+
+
+    // Binding back button on second step
+    $(".back2").click(function () {
+        $("#sf1").show("slow");
+    });
+
+    // Binding back button on third step
+    $(".back3").click(function () {
+        $("#sf2").show("slow");
+    });
+
+
 });
+
 
 
